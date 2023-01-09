65026d68f8fc3e5f69b4a65da12726e9fea8f7d0
yiannis <johndim99@gmail.com>
2023-01-09 22:01:46 +0200

    footer
    some design in footer

--- src/components/footerSection.vue
+++ src/components/footerSection.vue
@@ -1,63 +1,27 @@
 <template>
     <div class="w-full bg-black text-gray-100 tablet:bg-white tablet:text-black desktop:bg-black desktop:text-white">
 
-        <div 
-        class="
-        container
-        mx-auto
-        max-w-4xl
-        h-full
-        flex
-        flex-wrap
-        justify-between
-        items-start
-        text-sm
-        p-6
-        pt-8
-        pb-4"
-        >
-        <div class="mb-4">
-            <div class="mb-4">
-                Quick Links
-            </div>
-        </div>
-        <p class="mb-4 leading-normal">
-            Aristotelous Square 9 <br>
-        Thessaloniki <br>
-        546 23
-        </p>
-        <p>2310 555555</p>
-        </div>
-
-        <div class="">
-            
-        </div>
-
-        <div 
-        class="
-        container
-        mx-auto
-        max-w-4xl
-        h-full
-        flex
-        flex-wrap
-        justify-between
-        items-center
-        text-sm
-        py-6"
         
-        >
-            &copy;2023 August Company. All Rights Reserved
-            <div class="pt-4 tablet:p-0 text-center tablet:text-right text-xs">
-                <div class="no-underline hover:underline">Privacy Policy</div>
-                <div class="no-underline hover:underline ml-4">Terms & Conditions</div>
-                <div class="no-underline hover:underline ml-4">Contact Us</div>
+
+        <div class=" flex flex-col flex-nowrap gap-4 container mx-auto
+        max-w-4xl h-full justify-between items-center py-6">
+
+            <div class="footerlinks text-2xl">Join Our Newsletter</div>
+            <!--social media links-->
+            <div class="pt-4 tablet:p-0 text-center">
+
                 <a href="#instagram" class="social"><font-awesome-icon class="fa-brands fa-instagram" /></a>
                 <a href="#facebook" class="social"><font-awesome-icon class="fa-brands fa-facebook"  /></a>
                 <a href="#twitter" class="social"><font-awesome-icon class="fa-brands fa-twitter" /></a>
                 <a href="#pinterest" class="social"><font-awesome-icon class="fa-brands fa-pinterest" /></a>
+        
             </div>
+
+            <div class="footerlinks text-xl ">Do Not Sell My Personal Information</div>         
+            <div class="footerlinks text-lg ">Privacy and Cookies Policy</div>
+            <div class="footerlinks text-base ">Terms & Conditions</div>
         </div>
+        
     </div>
 </template>
 
