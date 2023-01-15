e87b19dfeed046301e41f37763b8768232ee2ca5
yiannis <johndim99@gmail.com>
2023-01-15 15:15:19 +0200

    when to show the bar menu
    i placed the mouseenter = false
    so that the bars wont get confused

--- src/App.vue
+++ src/App.vue
@@ -24,22 +24,25 @@
         </button>
     
         <!--bag-->
-        <button 
-        class="headerbtn">
-            <svg xmlns="http://www.w3.org/2000/svg" 
-            fill="none" 
-            viewBox="0 0 24 24" stroke-width="1.5" 
-            stroke="currentColor" 
-            class="headersvg">
-            <path stroke-linecap="round" 
-            stroke-linejoin="round" 
-            d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-        </button>
-            
+        <div class="relative" 
+        @mouseenter="showBar=false">
+            <button  
+            class="headerbtn">
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" stroke-width="1.5" 
+                stroke="currentColor" 
+                class="headersvg">
+                <path stroke-linecap="round" 
+                stroke-linejoin="round" 
+                d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                </svg>
+            </button>
+        </div>    
             
         <!--drop down menu-->
-        <div class="relative">
+        <div class="relative" @mouseover="showBar=true"
+        >
             <button
             @click="toggleBar"
             class="headerbtn"
@@ -57,8 +60,9 @@
                 />
                 </svg>
             </button>
-    
-            <div v-if="showBar" class="bar ">
+        </div>
+        <div class="relative" @mouseleave="toggleBar" >
+            <div v-if="showBar"  @click="toggleBar" class="bar ">
                 <a href="#sweats" class="dropdown">Sweats</a>
                 <a href="#shirts" class="dropdown">Shirts</a>
                 <a href="#pants" class="dropdown">Bottoms</a>
