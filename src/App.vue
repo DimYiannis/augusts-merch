a1ced94c4a3185d22f64ea30fe12960800589f4e
yiannis <johndim99@gmail.com>
2023-01-16 14:07:06 +0200

    conditional rendering
    in favorites too

--- src/App.vue
+++ src/App.vue
@@ -10,30 +10,42 @@
     items-end gap-1 h-10 bg-transparent">
         <!--favorites-->
         <button 
+        @mouseenter="showFavtip=true"
+        @mouseleave="showFavtip=false"
         class="headerbtn">
-            <svg xmlns="http://www.w3.org/2000/svg" 
-            fill="none" 
-            viewBox="0 0 24 24" 
-            stroke-width="1.5" 
-            stroke="currentColor" 
-            class="headersvg">
-            <path stroke-linecap="round" 
-            stroke-linejoin="round" 
-            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-            </svg>
+
+            <h1 v-if="showFavtip"
+            class="tooltip">Favorites</h1>
+
+            <h1 v-else
+            class="tooltip">
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                stroke-width="1.5" 
+                stroke="currentColor" 
+                class="headersvg">
+                <path stroke-linecap="round" 
+                stroke-linejoin="round" 
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                </svg>
+            </h1>
+            
         </button>
     
         <!--bag-->
         <!--i placed the mouseenter = false 
         so that the bars wont get confused-->
         <div @mouseenter="showBar=false"
-        @mouseover="showBag=true"
-        @mouseleave="showBag=false"
+        @mouseover="showBagtip=true"
+        @mouseleave="showBagtip=false"
         class="shoppingbag" >
             <button  
-            class="headerbtn hover:w-fit ">
+            class="headerbtn">
                 
-                <h1 v-if="showBag" class="tooltip">Shopping Bag</h1>
+            <!--conditional rendering in order to describe what 
+                the icon is like a tooltip short of speak-->
+                <h1 v-if="showBagtip" class="tooltip">Shopping Bag</h1>
                 <h1 v-else class="tooltip">
                     <svg xmlns="http://www.w3.org/2000/svg" 
                     fill="none" 
@@ -44,11 +56,7 @@
                     stroke-linejoin="round" 
                     d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                     </svg>
-                </h1>
-
-                
-
-               
+                </h1>  
             </button>
 
         </div>    
@@ -152,14 +160,14 @@ import sweats from './components/sweats.vue'
 import shirts from './components/shirts.vue'
 import bottoms from './components/bottoms.vue'
 import footerSection from './components/footerSection.vue'
-import MyTooltip from './components/MyTooltip.vue'
+
 
 
 export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'App',
     components: { Apparel,footerSection,bottoms,sweats,shirts, 
-    MyTooltip,},
+    },
     data() {
         return {
             showApparel: false, 
@@ -167,7 +175,8 @@ export default {
             showSweats:false,     
             showShirts:false,     
             showBottom:false,
-            showBag:false,     
+            showBagtip:false,
+            showFavtip:false     
         }
     },
     methods: {
@@ -186,9 +195,13 @@ export default {
         toggleBar() {
             this.showBar = !this.showBar
         },
-        toggleBag() {
-            this.showBag = !this.showBag
+        toggleBagtip() {
+            this.showBagtip = !this.showBagtip
         },
+        togglefavtip() {
+            this.showFavtip = !this.showFavtip
+        },
+        
     },
         
         
